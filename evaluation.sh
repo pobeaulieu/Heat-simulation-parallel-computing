94f10a679468d3f4901c926e73771dba2fb0d9f6
Pier-Olivier <pier-olivier.beaulieu.1@ens.etsmtl.ca>
2023-03-07 15:47:41 -0500

    Clean code

--- evaluation.sh
+++ evaluation.sh
@@ -8,12 +8,12 @@
 # echo "<<<"
 # mpirun -np 6 ./lab3 25 19 700 0.00025 0.1
 # echo ">>>"
-
+# 20 % comprend pas pk
 # echo "mpirun -np 64 ./lab3 1000 15 15 0.00025 0.1"
 # echo "<<<"
 # mpirun -np 64 ./lab3 1000 15 15 0.00025 0.1
 # echo ">>>"
-
+# TBD apres l'implementation col major
 # echo "mpirun -np 35 ./lab3 10 1200 35 0.00025 0.1"
 # echo "<<<"
 # mpirun -np 35 ./lab3 10 1200 35 0.00025 0.1
--- src/solver/solver.cpp
+++ src/solver/solver.cpp
@@ -56,50 +56,42 @@ void solveSeq(int rows, int cols, int iterations, double td, double h, int sleep
 void solvePar(int rows, int cols, int iterations, double td, double h, int sleep, double ** matrix, int rank, int lastRank) 
 {
     double c, l, r, t, b;
-    
     double h_square = h * h;
 
-    double * linePrevBuffer = new double[cols];
-    double * lineCurrBuffer = new double[cols];
-
+    // Communication buffers
     double * previousBufferReceive = new double[cols];
     double * nextBufferReceive = new double[cols];
 
+    // Calculation buffers
+    double * linePrevBuffer = new double[cols];
+    double * lineCurrBuffer = new double[cols];
+
+    // Previous iteration buffers. Useful to compute middle first
     double * firstLine = new double[cols];
     double * secondLine = new double[cols];
     double * beforeLastLine = new double[cols];
     double * lastLine = new double[cols];
 
-
     for(int k = 0; k < iterations; k++) {
+        // Copy borders to compute middle first
         memcpy(firstLine, matrix[0], cols * sizeof(double));
         memcpy(secondLine, matrix[1], cols * sizeof(double));
         memcpy(beforeLastLine, matrix[rows-2], cols * sizeof(double));
         memcpy(lastLine, matrix[rows-1], cols * sizeof(double));
 
-        // Send lines to other threads
-        if (rank == 0)
+        // Send buffers to other threads as soon as possible
+        if (rank != lastRank)
         {
             MPI_Request request;
             MPI_Isend(lastLine, cols, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &request);
         }
-        else if (rank == lastRank)
+        if (rank != 0)
         {
             MPI_Request request;
             MPI_Isend(firstLine, cols, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &request);
-
         }
-        else
-        {
-            // Send the first line of the previous iteration to rank - 1 
-            // Send the last line of the previous iteration to rank + 1
-            MPI_Request request1, request2;
-            MPI_Isend(firstLine, cols, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &request1);
-            MPI_Isend(lastLine, cols, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &request2);
-
-        }
-
-        // ComputeMiddle in each case, independently
+    
+        // ComputeMiddle in each case, independently of other threads
         memcpy(linePrevBuffer, firstLine, cols * sizeof(double));
         for(int i = 1; i < rows - 1; i++) {
             memcpy(lineCurrBuffer, matrix[i], cols * sizeof(double));
@@ -115,52 +107,10 @@ void solvePar(int rows, int cols, int iterations, double td, double h, int sleep
             memcpy(linePrevBuffer, lineCurrBuffer, cols * sizeof(double));
         }
 
-        // Edge cases
-        // Solve last line with received buffer
-        if (rank == 0)
+        // Compute edge lines
+        // Edge case 1 - Last line - Receive the next line if there is a neighbor at rank + 1
+        if (rank != lastRank)
         {
-            MPI_Recv(nextBufferReceive, cols, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            for(int j = 1; j < cols - 1; j++) {
-                    c = lastLine[j];
-                    t = beforeLastLine[j];
-                    b = nextBufferReceive[j];
-                    l = lastLine[j - 1];
-                    r = lastLine[j + 1];
-                    sleep_for(microseconds(sleep));
-                    matrix[rows-1][j] = c * (1.0 - 4.0 * td / h_square) + (t + b + l + r) * (td / h_square);
-            }
-        }
-       
-        else if (rank == lastRank)
-        {
-            MPI_Recv(previousBufferReceive, cols, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            for(int j = 1; j < cols - 1; j++) {
-                c = firstLine[j];
-                t = previousBufferReceive[j];
-                b = secondLine[j];
-                l = firstLine[j - 1];
-                r = firstLine[j + 1];
-                sleep_for(microseconds(sleep));
-                matrix[0][j] = c * (1.0 - 4.0 * td / h_square) + (t + b + l + r) * (td / h_square);
-            }
-        }
-        // Solve first AND last line with received buffer 
-        else
-        {
-            // Receive line from rank - 1 to compute first line
-            MPI_Recv(previousBufferReceive, cols , MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            for(int j = 1; j < cols - 1; j++) {
-                c = firstLine[j];
-                t = previousBufferReceive[j];
-                b = secondLine[j];
-                l = firstLine[j - 1];
-                r = firstLine[j + 1];
-
-                sleep_for(microseconds(sleep));
-                matrix[0][j] = c * (1.0 - 4.0 * td / h_square) + (t + b + l + r) * (td / h_square);
-            }
-
-            // Receive line from rank + 1 to compute last line
             MPI_Recv(nextBufferReceive, cols, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             for(int j = 1; j < cols - 1; j++) {
                 c = lastLine[j];
@@ -172,7 +122,20 @@ void solvePar(int rows, int cols, int iterations, double td, double h, int sleep
                 matrix[rows-1][j] = c * (1.0 - 4.0 * td / h_square) + (t + b + l + r) * (td / h_square);
             }
         }
-         
+        // Edge case 2 - First line -  Receive the previous line if there is a neighbor at rank - 1
+        if (rank != 0)
+        {
+            MPI_Recv(previousBufferReceive, cols, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            for(int j = 1; j < cols - 1; j++) {
+                c = firstLine[j];
+                t = previousBufferReceive[j];
+                b = secondLine[j];
+                l = firstLine[j - 1];
+                r = firstLine[j + 1];
+                sleep_for(microseconds(sleep));
+                matrix[0][j] = c * (1.0 - 4.0 * td / h_square) + (t + b + l + r) * (td / h_square);
+            }
+        }  
     }
 
     delete[](linePrevBuffer);
